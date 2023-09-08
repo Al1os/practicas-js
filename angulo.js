d467dcb48bc5b492534a4d72c011f81d0fbb9bb5
unknown <saulalejandrorojasr@gmail.com>
2023-09-08 09:35:37 -0400

    ejercicio 3 de js

--- angulo.js
+++
@@ -1,5 +0,0 @@
-vueltas = Math.floor(Math.random() * (11 - 5) + 5);
-angulo = Math.floor(Math.random() * (360 - 0) + 0)
-total = Math.floor(Math.random() * (360-0) + 0) * vueltas + angulo;
-final = total % 360;
-console.log("Angulo: "+ angulo + "\nNro aleatorio: " + total + "\n" + "nro angulo final: " + final + "\n");
